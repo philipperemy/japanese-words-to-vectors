0209124408e815ffb4526834a91b3b022935a6e5
Philippe Remy <premy@reactive.co.jp>
2016-09-05 18:28:30 +0900

    clean and run

---
+++ clean_and_run.sh
@@ -0,0 +1,7 @@
+rm jawiki-latest-text.txt
+rm jawiki-latest-text-sentences.txt
+rm jawiki-latest-text-tokens.txt
+rm jawiki-latest-text-sentences-tokens.txt
+rm ja-gensim.50d.data.model
+rm ja-gensim.50d.data.txt
+python generate_vectors.py
