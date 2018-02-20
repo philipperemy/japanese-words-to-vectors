b715726bbe9e8162b43340e1e0b8b189c230611d
Markus Opolka <markus.opolka@fau.de>
2018-02-20 12:30:21 +0100

    Refactor File/UTF8 handling
    - Basically removed all explicit encoding to make this better work
      with Python3
    - Also included a small wiki dump to test the script

--- generate_vectors.py
+++ generate_vectors.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 import argparse
@@ -42,75 +42,106 @@ JA_VECTORS_MODEL_FILENAME = 'ja-gensim.{}d.data.model'.format(VECTORS_SIZE)
 JA_VECTORS_TEXT_FILENAME = 'ja-gensim.{}d.data.txt'.format(VECTORS_SIZE)
 JA_WIKI_LATEST_URL = 'https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles.xml.bz2'
 
-
 # CHECK WHERE THE HECK ARE THE PUNCTUATIONS GONE. Okay it's in get_text()
 # WHY WE DO NOT HAVE ANY OUTPUT ON WORD2VEC. We have to define a logging interface
 
 def generate_vectors(input_filename, output_filename, output_filename_2):
+
     if os.path.isfile(output_filename):
         logging.info('Skipping generate_vectors(). File already exists: {}'.format(output_filename))
         return
+
     start = time.time()
+
     model = Word2Vec(LineSentence(input_filename),
-                     size=VECTORS_SIZE, window=5, min_count=5,
-                     workers=cpu_count(), iter=5)
+                     size=VECTORS_SIZE,
+                     window=5,
+                     min_count=5,
+                     workers=4,
+                     iter=5)
+
     model.save(output_filename)
     model.wv.save_word2vec_format(output_filename_2, binary=False)
+
     logging.info('Finished generate_vectors(). It took {0:.2f} s to execute.'.format(round(time.time() - start, 2)))
 
 
 def get_words(text):
+
     import MeCab
-    mt = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-    mt.parse("")
+    mt = MeCab.Tagger('-d /usr/lib/mecab/dic/mecab-ipadic-neologd')
+
+    mt.parse('')
+
     parsed = mt.parseToNode(text)
     components = []
+
     while parsed:
         components.append(parsed.surface)
         parsed = parsed.next
+
     return components
 
 
 def tokenize_text(input_filename, output_filename):
+
     if os.path.isfile(output_filename):
         logging.info('Skipping tokenize_text(). File already exists: {}'.format(output_filename))
         return
+
     start = time.time()
-    with open(output_filename, 'wb') as out:
-        with open(input_filename, 'rb') as inp:
+
+    with open(output_filename, 'w') as out:
+        with open(input_filename, 'r') as inp:
+
             for i, text in enumerate(inp.readlines()):
+
                 if USE_MECAB_TOKENIZER:
-                    tokenized_text = ' '.join(get_words(text.decode('utf8')))
+                    tokenized_text = ' '.join(get_words(text))
                 else:
-                    tokenized_text = ' '.join(tinysegmenter.tokenize(text.decode('utf8')))
-                out.write(tokenized_text.encode('utf8'))
+                    tokenized_text = ' '.join(tinysegmenter.tokenize(text))
+
+                out.write(tokenized_text)
+
                 if i % 100 == 0 and i != 0:
                     logging.info('Tokenized {} articles.'.format(i))
     logging.info('Finished tokenize_text(). It took {0:.2f} s to execute.'.format(round(time.time() - start, 2)))
 
 
 def process_wiki_to_text(input_filename, output_text_filename, output_sentences_filename):
+
     if os.path.isfile(output_text_filename) and os.path.isfile(output_sentences_filename):
         logging.info('Skipping process_wiki_to_text(). Files already exist: {} {}'.format(output_text_filename,
                                                                                           output_sentences_filename))
         return
+
     start = time.time()
     intermediary_time = None
     sentences_count = 0
-    with open(output_text_filename, 'wb') as out:
-        with open(output_sentences_filename, 'wb') as out_sentences:
+
+    with open(output_text_filename, 'w') as out:
+        with open(output_sentences_filename, 'w') as out_sentences:
+
+            # Open the Wiki Dump with gensim
             wiki = WikiCorpus(input_filename, lemmatize=False, dictionary={}, processes=cpu_count())
             wiki.metadata = True
             texts = wiki.get_texts()
+
             for i, article in enumerate(texts):
-                text_list = article[0]  # article[1] refers to the name of the article.
+                # article[1] refers to the name of the article.
+                text_list = article[0]  
                 sentences = text_list
-                # re.search('[a-zA-Z]+', string) maybe add it
                 sentences_count += len(sentences)
+
+                # Write sentences per line
                 for sentence in sentences:
-                    out_sentences.write((sentence + b'\n'))
-                text = b' '.join(sentences) + b'\n'
+                    out_sentences.write((sentence + '\n'))
+
+                # Write each page in one line
+                text = ' '.join(sentences) + '\n'
                 out.write(text)
+
+                # This is just for the logging
                 if i % (100 - 1) == 0 and i != 0:
                     if intermediary_time is None:
                         intermediary_time = time.time()
@@ -128,6 +159,7 @@ def process_wiki_to_text(input_filename, output_text_filename, output_sentences_
 
 
 if __name__ == '__main__':
+
     if not os.path.isfile(INPUT_FILENAME):
         wget.download(JA_WIKI_LATEST_URL)
 
