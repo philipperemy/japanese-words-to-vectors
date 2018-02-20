3c16067802b46b2044448b85d3ccf9e556e650e3
Markus Opolka <markus.opolka@fau.de>
2018-02-20 13:18:37 +0100

    Update Cleanup Script
    - Removed run on generate script
    - Added wildcard for model files

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,6 @@ rm -f jawiki-latest-text.txt
 rm -f jawiki-latest-text-sentences.txt
 rm -f jawiki-latest-text-tokens.txt
 rm -f jawiki-latest-text-sentences-tokens.txt
-rm -f ja-gensim.50d.data.model
-rm -f ja-gensim.50d.data.txt
+rm -f ja-gensim.*.data.model
+rm -f ja-gensim.*.data.txt
 rm -rf *.npy
-python3 generate_vectors.py --mecab
